0849bdac0afed89d3dc1a04d5bd3fdcdeb563f84
GoGu <hugo.roche48@gmail.com>
2025-04-17 23:07:55 +0200

    [FIX] - drag & drop reading for load command

--- src/Commands/Load.cpp
+++ src/Commands/Load.cpp
@@ -6,6 +6,15 @@ namespace MKTG::Command
 
 Result Load::run(MKTG::Generator &gen, std::string args)
 {
+    for (auto it = args.begin(); it != args.end(); it++) {
+        if (*it == '"') {
+            it = args.erase(it);
+            if (it == args.end()) {
+                break;
+            }
+        }
+    }
+
     gen.Load(args);
     return Result::CONTINUE;
 }
