1e577bef5e218e8903563ed483cb419aad3eaa74
GoGu <hugo.roche48@gmail.com>
2025-10-07 15:31:39 +0200

    [FIX] - remove bracket player list loading

--- src/Data/Data.cpp
+++ src/Data/Data.cpp
@@ -12,12 +12,6 @@ void loadData()
     GNCApp::Data::Assets::getInstance();
     GNCApp::Data::Winner::getInstance();
     GNCApp::Data::Seeding::getInstance();
-
-    GNCApp::Data::Seeding::getInstance()->addPlayer("Groluche");
-    GNCApp::Data::Seeding::getInstance()->addPlayer("Batefimbi");
-    GNCApp::Data::Seeding::getInstance()->addPlayer("GoGu");
-    GNCApp::Data::Seeding::getInstance()->addPlayer("Supnyus");
-    GNCApp::Data::Seeding::getInstance()->addPlayer("SewLink");
 }
 
 } // namespace GNCApp::Data
