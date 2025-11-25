37fd2b3604d48f4c63e755d98ce4ff54121ca3bd
GoGu <hugo.roche48@gmail.com>
2025-11-25 16:11:36 +0100

    [FIX] - current version

--- include/Update.hpp
+++ include/Update.hpp
@@ -2,6 +2,6 @@
 #pragma once
 
 #define GNCAPP_NAME         "GamenChillApp"
-#define GNCAPP_VERSION      "v3.3.0"
+#define GNCAPP_VERSION      "v3.3.1"
 #define GNCAPP_TARGET       "GamenChillApp_WIN32.zip"
 #define GNCAPP_GITHUB_URL   "https://api.github.com/repos/Game-N-Chill/GamenChillApp/releases/latest"
